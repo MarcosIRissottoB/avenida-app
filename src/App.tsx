13adb9684757b7d06b80bb1c6dd3f7eca19c3c6b
MarcosIRissottoB <marcosrissottob@gmail.com>
2024-05-23 15:38:54 -0300

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
     <>
+
+    
       <h1>Avenida client</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
